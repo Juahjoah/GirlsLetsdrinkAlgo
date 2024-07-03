f605197ac66555bed87bf81498dabaa087cd5ade
Juahjoah <07juah21@gmail.com>
2024-07-03 22:32:52 +0900

    ✨✏️ comment: PGS_중복_제거하기 주석 추가

--- Juahjoah/_240703/PGS_중복_제거하기.sql
+++ Juahjoah/_240703/PGS_중복_제거하기.sql
@@ -2,3 +2,8 @@
 -- SELECT DISTINCT COUNT(ANIMAL_ID) AS COUNT
 SELECT COUNT(DISTINCT NAME) AS COUNT
   FROM ANIMAL_INS;
+
+-- COUNT 함수는 동일하게 사용하지만, Null을 제외하기 위해서는 원하는 컬럼명을 정확하게 지정해야 한다.
+-- DISTINCT는 중복을 제거하는 역할을 한다. 다만, 중복을 제거할 때는 NULL 값도 중복으로 간주한다.
+  -- DISTINCT : 중복을 없애주지만 정렬을 하지는 않는다.
+  -- GROUP BY : 중복을 없애주고 정렬도 해준다.
