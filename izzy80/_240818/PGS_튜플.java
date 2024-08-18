367897ab5881e5a053e03cc619a9791d30ced96a
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-08-18 23:36:45 +0900

    ✨ feat:PGS_튜플 알고리즘 구현

---
+++ izzy80/_240818/PGS_튜플.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    class Num {
+        int number;
+        int cnt; 
+        
+        public Num(int number, int cnt){
+            this.number = number; 
+            this.cnt = cnt; 
+        }
+    }
+    public int[] solution(String s) {
+        int[] answer = {};
+        s = s.replaceAll("[\\{\\}]","");
+        String[] numbers = s.split(",");
+        
+        
+        
+        
+        return answer;
+    }
+}
